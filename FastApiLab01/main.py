10194e5e3588025d655874c811464d29f38f528b
Rely Ky <relyky@hotmail.com>
2023-05-08 15:39:19 +0800

    routing samle

--- FastApiLab01/main.py
+++ FastApiLab01/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hello world. 哈囉世界。"}
+
+@app.get('/book/{book_id}')
+def get_book_by_id(book_id: int):
+    return {
+        'book_id': book_id
+    }
